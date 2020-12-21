daaa0b89bb8622d9669a35f029cdf6684b1d423b
Tom Reeb <reebzor@gmail.com>
2020-12-20 20:40:55 -0500

    x730 -> x735

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# x730-script
+# x735-script
 
-This is a fork of the safe shutdown script for x730 from [Geekworm](https://github.com/geekworm-com/x730-script). This has been modified to be compatible with Ubuntu 20.04. The main difference is this script uses systemd in favor of rc.local.
+This is a fork of the safe shutdown script for x735 from [Geekworm](https://github.com/geekworm-com/x735-script). This has been modified to be compatible with x735 on Ubuntu 20.04. The main difference is this script uses systemd in favor of rc.local.
 
 ## Installation
 
 One-liner:
 
-`wget https://raw.githubusercontent.com/tomreeb/x730-script/master/x730.sh | xargs sudo bash`
+`wget https://raw.githubusercontent.com/tomreeb/x735-script/master/x735.sh | xargs sudo bash`
 
 Even though it's easy, you should not run that command above. Instead, download the script and review it before executing it:
 
 ```bash
-wget https://raw.githubusercontent.com/tomreeb/x730-script/master/x730.sh
-sudo chmod +x x730.sh
-sudo ./x730.sh
+wget https://raw.githubusercontent.com/tomreeb/x735-script/master/x735.sh
+sudo chmod +x x735.sh
+sudo ./x735.sh
 ```
 
 When the install is finished, reload your bash session.
@@ -22,5 +22,5 @@ When the install is finished, reload your bash session.
 
 ```bash
 $ sudo shutdown
-X730 Shutting down...
+x735 Shutting down...
 ```
--- x735.sh
+++ x735.sh
@@ -6,7 +6,7 @@ if [ "$(whoami)" != "root" ] ; then
 	exit 1
 fi
 
-#x730 Powering on /reboot /shutdown from hardware
+#x735 Powering on /reboot /shutdown from hardware
 
 echo '#!/bin/bash
 
@@ -20,7 +20,7 @@ echo "$BOOT" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio$BOOT/direction
 echo "1" > /sys/class/gpio/gpio$BOOT/value
 
-echo "X730 Shutting down..."
+echo "x735 Shutting down..."
 
 while [ 1 ]; do
   shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
@@ -31,23 +31,23 @@ while [ 1 ]; do
     while [ $shutdownSignal = 1 ]; do
       /bin/sleep 0.02
       if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMAXIMUM ]; then
-        echo "X730 Shutting down", SHUTDOWN, ", halting Rpi ..."
+        echo "x735 Shutting down", SHUTDOWN, ", halting Rpi ..."
         sudo poweroff
         exit
       fi
       shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
     done
     if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMINIMUM ]; then 
-      echo "X730 Rebooting", SHUTDOWN, ", recycling Rpi ..."
+      echo "x735 Rebooting", SHUTDOWN, ", recycling Rpi ..."
       sudo reboot
       exit
     fi
   fi
-done' > /usr/local/bin/x730pwr.sh
+done' > /usr/local/bin/x735pwr.sh
 
-chmod +x /usr/local/bin/x730pwr.sh
+chmod +x /usr/local/bin/x735pwr.sh
 
-#X730 full shutdown through Software
+#x735 full shutdown through Software
 
 echo '#!/bin/bash
 
@@ -64,39 +64,39 @@ if ! [[ $SLEEP =~ $re ]] ; then
    echo "error: sleep time not a number" >&2; exit 1
 fi
 
-echo "X730 Shutting down..."
+echo "x735 Shutting down..."
 /bin/sleep $SLEEP
 
 #restore GPIO 18
 echo "0" > /sys/class/gpio/gpio$BUTTON/value
-' > /usr/local/bin/x730shutdown.sh
+' > /usr/local/bin/x735shutdown.sh
 
-chmod +x /usr/local/bin/x730shutdown.sh
+chmod +x /usr/local/bin/x735shutdown.sh
 
 # Systemd Install Script
 
 echo "#!/bin/bash
 [Unit]
-Description=Geekworm x730 Power Management Script
+Description=Geekworm x735 Power Management Script
 
 Wants=network.target
 After=syslog.target network-online.target
 
 [Service]
 Type=simple
-ExecStart=/usr/local/bin/x730pwr.sh
+ExecStart=/usr/local/bin/x735pwr.sh
 Restart=on-failure
 RestartSec=10
 KillMode=process
 
 [Install]
 WantedBy=multi-user.target
-" > /etc/systemd/system/x730pwr.service
+" > /etc/systemd/system/x735pwr.service
 
 systemctl daemon-reload
-systemctl enable x730pwr.service --now
+systemctl enable x735pwr.service --now
 
 # Setup bash alias for software shutdown
 
-echo "alias shutdown='/usr/local/binx730shutdown.sh'
+echo "alias shutdown='/usr/local/binx735shutdown.sh'
 " > /etc/profile.d/00-aliases.sh
